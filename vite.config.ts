7a131fd90767f566d38a801cbe4b6db6a7d47fe9
Alice Wu <alice@macbook.local>
2026-04-26 11:05:37 -0700

    Switch to cloudflare-pages preset

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
-    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    nitro({ preset: 'cloudflare-pages', rollupConfig: { external: [/^@sentry\//] } }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
